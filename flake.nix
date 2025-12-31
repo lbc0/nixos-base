ab524d592022fa8d70abf374433ca438e64b559b
lbc0 <67844946+lbc0@users.noreply.github.com>
2025-12-31 14:52:38 +0100

    Add initial NixOS configuration in flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Lbc NixOS Config!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos-lbc = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.lbc = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
+}
